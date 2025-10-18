a0fda57290d1ad3fd82d9a32d50a8dc8bd091f75
shritej6 <gundas@msoe.edu>
2025-10-17 21:18:34 -0500

    updating Score

--- src/coconuts/Beach.java
+++ src/coconuts/Beach.java
@@ -1,12 +1,9 @@
 package coconuts;
 
-// the beach catches (hits) coconuts and increases the coconut score
-// This is a domain class; do not introduce JavaFX or other GUI components here
 public class Beach extends HittableIslandObject {
 
     public Beach(OhCoconutsGameManager game, int skyHeight, int islandWidth) {
         super(game, 0, skyHeight, islandWidth, null);
-        // System.out.println("Beach at y = " + this.y);
     }
 
     @Override
